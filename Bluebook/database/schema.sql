251a77edb114efcec54a3c7fdec2a891252245c6
Arthur Santos <145377096+Arthur-Snts@users.noreply.github.com>
2025-01-20 20:36:42 -0300

    Update schema.sql

--- Bluebook/database/schema.sql
+++ Bluebook/database/schema.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS tb_users (
     use_id INTEGER PRIMARY KEY AUTOINCREMENT,
     use_nome TEXT NOT NULL,
     use_email TEXT NOT NULL,
-    use_telefone INT NOT NULL,
-    use_endereço VARCHAR(90) NOT NULL
+    use_telefone VARCHAR(200) NOT NULL,
+    use_endereço VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tb_books (
@@ -15,15 +15,12 @@ CREATE TABLE IF NOT EXISTS tb_books (
     boo_autor TEXT NOT NULL,
     boo_genero TEXT NOT NULL,
     boo_isbn VARCHAR(90) NOT NULL,
-    boo_quant_estoque INT NOT NULL,
-    boo_use_id INTEGER NOT NULL,
-
-    FOREIGN KEY(boo_use_id) REFERENCES tb_users(use_id)
+    boo_quant_estoque INT NOT NULL
 );
 
 CREATE TABLE IR NOT EXISTS tb_lending(
     len_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    len_data_emprestimo DATE NOT NULL,
+    len_data_emprestimo DATE DEFAULT(CURDATE()) NOT NULL,
     len_data_devolucao DATE NOT NULL,
     len_boo_id INT NOT NULL,
     len_use_id INT NOT NULL,
