9e37a026a4d13e2483d60babe57e8cb2d2031935
Mateus Lucena <150374285+Mathewxs21@users.noreply.github.com>
2025-03-02 08:29:49 -0300

    Update schema.sql

--- Bluebook/database/schema.sql
+++ Bluebook/database/schema.sql
@@ -1,17 +1,23 @@
 CREATE DATABASE db_bluebook;
-USE db_bluebook;
+use db_bluebook;
 
+CREATE TABLE IF NOT EXISTS tb_leitores (
+    lei_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    lei_nome TEXT NOT NULL,
+    lei_email TEXT NOT NULL,
+    lei_telefone VARCHAR(200) NOT NULL,
+    lei_endereço VARCHAR(200) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS tb_users (
-    use_id INTEGER PRIMARY KEY auto_increment,
+    use_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     use_nome TEXT NOT NULL,
-    use_email TEXT NOT NULL,
-    use_telefone VARCHAR(200) NOT NULL,
-    use_endereco VARCHAR(200) NOT NULL
+    use_senha TEXT NOT NULL,
+    use_tipo TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tb_books (
-    boo_id INTEGER PRIMARY KEY auto_increment,
+    boo_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     boo_titulo TEXT NOT NULL,
     boo_autor TEXT NOT NULL,
     boo_genero TEXT NOT NULL,
@@ -20,13 +26,13 @@ CREATE TABLE IF NOT EXISTS tb_books (
 );
 
 CREATE TABLE IF NOT EXISTS tb_lending(
-    len_id INTEGER PRIMARY KEY auto_increment,
-    len_data_emprestimo DATE DEFAULT(CURDATE()) NOT NULL,
+    len_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    len_data_emprestimo DATE NOT NULL,
     len_data_devolucao DATE NOT NULL,
-    len_preco FLOAT NOT NULL,
-    len_boo_id INTEGER NOT NULL,
-    len_use_id INTEGER NOT NULL,
-
-    FOREIGN KEY (len_boo_id) REFERENCES tb_books(boo_id),
+    len_valor FLOAT,
+    len_boo_id INT NOT NULL,
+    len_use_id INT NOT NULL,
+    
+	FOREIGN KEY (len_boo_id) REFERENCES tb_books(boo_id),
     FOREIGN KEY (len_use_id) REFERENCES tb_users(use_id)
 );
