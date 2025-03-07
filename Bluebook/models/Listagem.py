b8ebd3c9a907f191a91ed82f8e39face81049049
AlvesArthur1983 <arturpatriciocaico@gmail.com>
2025-03-06 21:54:45 -0300

    ajeitando listagens

--- Bluebook/models/Listagem.py
+++ Bluebook/models/Listagem.py
@@ -7,7 +7,7 @@ class Listagem():
     def um(cls, use_id,data1, data2):
         conn = obter_conexao()  
         cursor = conn.cursor(dictionary=True)
-        query = f"SELECT use_nome, sum(len_preco) as soma FROM tb_lending JOIN tb_users ON len_use_id = use_id WHERE use_id = {use_id} and (len_data_emprestimo between '{data1}' and '{data2}') "
+        query = f"SELECT lei_nome, sum(len_valor) as soma FROM tb_lending JOIN tb_leitor ON len_lei_id = lei_id WHERE lei_id = {use_id} and (len_data_emprestimo between '{data1}' and '{data2}') "
         cursor.execute(query)
         total_emprestimos = cursor.fetchall()
         cursor.close()
@@ -17,7 +17,7 @@ class Listagem():
     def dois(cls, data1, data2):
         conn = obter_conexao()  
         cursor = conn.cursor(dictionary=True)
-        query = f"select use_nome, sum(len_preco) as soma from tb_lending join tb_users on len_use_id = use_id where len_data_emprestimo between '{data1}' and '{data2}' group by use_nome having sum(len_preco) > 100"
+        query = f"select lei_nome, sum(len_valor) as soma from tb_lending join tb_leitores on len_lei_id = lei_id where len_data_emprestimo between '{data1}' and '{data2}' group by lei_nome having sum(len_valor) > 100"
         cursor.execute(query)
         usuarios = cursor.fetchall()
         cursor.close()
